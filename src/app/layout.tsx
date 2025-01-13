653830d95631cf8f0f1f27286e803144fe88a1d4
Ayesha788 <164652720+Ayesha788@users.noreply.github.com>
2025-01-13 22:00:46 +0500

    initial comment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,17 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "./components/navbar";
+import Footer from "./components/footer";
+import Bottom from "./components/bottom";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+
+ 
+
+
+const inter = Inter({ subsets: ["latin"] });
+
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,12 +24,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en" className="max-w-[1920px]">
+      
+      <body className={inter.className} >
+        <Header />
         {children}
-      </body>
+        <Footer />
+        <Bottom />
+        </body>
     </html>
   );
 }
+ 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily:{
+        greatvibes:['"Great Vibes"','cursive'],
+      }
     },
   },
   plugins: [],
